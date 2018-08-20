66b1411d29a75d36cb4bf7b0b1a9fee98fce8397
Kevin Nderitu <nderitukelvin19@gmail.com>
2018-08-20 17:19:40 +0300

    [build] 0.1.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,26 +6,24 @@ read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]] 
 then
-    #echo "Releasing $VERSION ..."
-
     BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-    read -p "Are you sure you want to release on $BRANCH branch? (y/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        echo "Releasing v$VERSION on branch $BRANCH ..."
+    if [[ $BRANCH == 'dev' ]]
+    then 
+        echo "Releasing v$VERSION ..."
         # commit
         yarn build
         echo
         git add -A
         git commit -m "[build] $VERSION"
-        git push -u origin $BRANCH
+        git push origin $BRANCH
+        git checkout master
+        git merge $BRANCH
+        git push -u origin master
         git tag -a v$VERSION -m "[BUILD] $VERSION"
         
         # publish
         git push origin --tags
-        URL=`git remote get-url origin`
-        git request-pull v$VERSION $URL master
-        git push
+    else 
+        echo "Release cannot be done on '$BRANCH' branch. Please switch to 'dev' branch to release."
     fi
 fi
