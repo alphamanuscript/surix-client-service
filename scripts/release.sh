392688f9b75c1cc05a144b87bb5f47908c6f4b08
Kevin Nderitu <nderitukelvin19@gmail.com>
2018-08-20 16:28:52 +0300

    [build] 0.2.0

---
+++ scripts/release.sh
@@ -0,0 +1,27 @@
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]] 
+then
+    #echo "Releasing $VERSION ..."
+
+    BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+    read -p "Are you sure you want to release on $BRANCH branch? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Releasing v$VERSION on branch $BRANCH ..."
+        # commit
+        yarn build
+        git add -A
+        git commit -m "[build] $VERSION"
+
+        
+        # publish
+        #git push origin refs/tags/v$VERSION
+        #git push
+    fi
+fi
